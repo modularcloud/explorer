3d995e1359728f694327edf28524a94e4bc07807
Raeanne Villanueva <raeanne.villanueva@gmail.com>
2023-02-14 19:34:14 +0800

    added props for card

--- packages/@modularcloud/design-system/components/card/index.tsx
+++ packages/@modularcloud/design-system/components/card/index.tsx
@@ -1,29 +1,39 @@
-export function Card() {
-  const entryLabels = [
-    "Index",
-    "Chain ID",
-    "Height",
-    "Status",
-    "Time",
-    "Fee",
-    "Gas (used/wanted)",
-    "Messages",
-  ];
-  const entryData = [
-    "Chain ID",
-    "Transactions",
-    "Height",
-    "Block time",
-    "Block time",
-    "Gas (used/wanted)",
-    "Block Round",
-    "Transactions",
-  ];
+import { CircleButton } from "../circle-button";
+import { CubesOn, ArrowOn } from "../../icons";
 
+const entryLabels = [
+  "Index",
+  "Chain ID",
+  "Height",
+  "Status",
+  "Time",
+  "Fee",
+  "Gas (used/wanted)",
+  "Messages",
+];
+const entryData = [
+  "Chain ID",
+  "Transactions",
+  "Height",
+  "Block time",
+  "Block time",
+  "Gas (used/wanted)",
+  "Block Round",
+  "Transactions",
+];
+
+interface Props {
+  type: string;
+  entries?: Array<[key: string, value: string]>;
+  badgeText?: string;
+  badgeIcon?: string;
+}
+
+export function Card({ badgeText }: Props) {
   return (
-    <div className="border shadow-md rounded-lg">
+    <div className="w-full border shadow-md rounded-lg">
       <div className="bg-slate-100">Transaction</div>
-      <div></div>
+      <CircleButton>{badgeText}</CircleButton>
     </div>
   );
 }
