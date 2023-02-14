d7fdd0bd4c3d3a90e242c5d77a36e1b7c0716950
Raeanne Villanueva <raeanne.villanueva@gmail.com>
2023-02-14 16:05:37 +0800

    resolved dialog problems when changing viewport

--- packages/@modularcloud/design-system/components/dialog-panel/index.tsx
+++ packages/@modularcloud/design-system/components/dialog-panel/index.tsx
@@ -7,6 +7,8 @@ interface Props {
   triggerIcon: React.ReactNode;
   children?: React.ReactNode;
   contentClass: string;
+  btnClass?: string;
+  overlayClass?: string;
   removeCloseBtn?: boolean;
 }
 
@@ -14,17 +16,22 @@ export function DialogPanel({
   children,
   triggerIcon,
   contentClass,
+  overlayClass,
+  btnClass,
   removeCloseBtn,
 }: Props) {
   return (
     <Dialog.Root>
       <Dialog.Trigger asChild>
-        <button className="h-7 flex justify-center items-center rounded-full border-mid-dark-100 border p-1 shadow-[0px_3px_6px_rgba(42,43,46,0.07),0px_1px_2px_rgba(42,43,46,0.04)]">
-          {triggerIcon}
-        </button>
+        <button className={btnClass}>{triggerIcon}</button>
       </Dialog.Trigger>
       <Dialog.Portal>
-        <Dialog.Overlay className="fixed inset-0 bg-night/50 backdrop-blur-sm" />
+        <Dialog.Overlay
+          className={clsx(
+            "fixed inset-0 bg-night/50 backdrop-blur-sm",
+            overlayClass
+          )}
+        />
         <Dialog.Content className={contentClass}>
           {children}
           <Dialog.Close asChild>
--- packages/@modularcloud/design-system/components/header/index.tsx
+++ packages/@modularcloud/design-system/components/header/index.tsx
@@ -20,6 +20,7 @@ export function Header({ panelContent, searchInput }: Props) {
           <ViewSwitcher />
           <div className="flex gap-4 items-center lg:hidden">
             <DialogPanel
+              btnClass="h-7 flex justify-center items-center rounded-full border-mid-dark-100 border p-1 shadow-[0px_3px_6px_rgba(42,43,46,0.07),0px_1px_2px_rgba(42,43,46,0.04)]"
               contentClass="block fixed top-12 inset-x-0 animate-fadeIn"
               triggerIcon={<SearchOff />}
               removeCloseBtn
@@ -28,7 +29,9 @@ export function Header({ panelContent, searchInput }: Props) {
             </DialogPanel>
 
             <DialogPanel
-              contentClass="block fixed top-0 right-0 w-3/4 md:w-4/6 h-full animate-rtl"
+              btnClass="h-7 flex justify-center items-center rounded-full border-mid-dark-100 border p-1 shadow-[0px_3px_6px_rgba(42,43,46,0.07),0px_1px_2px_rgba(42,43,46,0.04)]"
+              contentClass="block fixed top-0 right-0 w-3/4 md:w-4/6 h-full animate-rtl block lg:hidden"
+              overlayClass="block lg:hidden"
               triggerIcon={<RightSidebarOff />}
             >
               {panelContent}
