b368cd9dc9882d6729b9000fee9e0087af99d133
Raeanne Villanueva <raeanne.villanueva@gmail.com>
2023-02-15 19:00:54 +0800

    added open handler for the popover, fixing build errors

--- apps/web/pages/[networkLabel]/[entityType]/[field]/[fieldValue].tsx
+++ apps/web/pages/[networkLabel]/[entityType]/[field]/[fieldValue].tsx
@@ -16,25 +16,21 @@ import {
   CardList,
   Table,
 } from "@modularcloud/design-system";
-import Image from 'next/image';
+import Image from "next/image";
 
 import { CubesOff } from "@modularcloud/design-system";
 import { SearchOptions } from "../../../../lib/search-options";
 
 interface PanelProps {
   classes: string;
-  type: string,
-  id: string,
-  metadata: { [key: string]: string }
+  type: string;
+  id: string;
+  metadata: { [key: string]: string };
 }
 
 const EntityPanel = ({ classes, type, id, metadata }: PanelProps) => (
   <RightPanel className={classes}>
-    <EntityDetails
-      iconType={<CubesOff />}
-      type={type}
-      hash={id}
-    />
+    <EntityDetails iconType={<CubesOff />} type={type} hash={id} />
     <KeyValueList
       header="Block Information"
       entryLabels={Object.keys(metadata)}
@@ -86,7 +82,7 @@ export const getServerSideProps: GetServerSideProps<{
     );
   }
 
-  if(entity.computed.Messages) {
+  if (entity.computed.Messages) {
     associated.push(...entity.computed.Messages);
   }
 
@@ -105,18 +101,41 @@ function EntityPage({
   const [selectedItem, setSelectedItem] = useState(
     SearchOptions[0].options[0].value
   );
-  const [view, setView] = useState(entity.context.entityTypeName === "Transaction" || associated.length < 3 ? "cards" : "table");
+
+  const [openPopover, setOpenPopover] = useState(false);
+
+  const [view, setView] = useState(
+    entity.context.entityTypeName === "Transaction" || associated.length < 3
+      ? "cards"
+      : "table"
+  );
 
   const handleSelect = (selectedItem: React.SetStateAction<string>) => {
     console.log(selectedItem);
     setSelectedItem(selectedItem);
   };
 
+  const handleOpen = (openPopover: React.SetStateAction<boolean>) => {
+    // set to true if no search results
+    openPopover = false;
+    setOpenPopover(openPopover);
+  };
+
   return (
     <div className="flex">
       <div className="grow">
         <div className="lg:hidden">
-          <TopBar type={entity.context.entityTypeName} id={entity.uniqueIdentifier}><Image src="/images/Celestia-icon-logo.png" alt="Celestia" height="32" width="119" /></TopBar>
+          <TopBar
+            type={entity.context.entityTypeName}
+            id={entity.uniqueIdentifier}
+          >
+            <Image
+              src="/images/Celestia-icon-logo.png"
+              alt="Celestia"
+              height="32"
+              width="119"
+            />
+          </TopBar>
         </div>
         <Header
           searchInput={
@@ -126,25 +145,48 @@ function EntityPage({
               optionGroups={SearchOptions}
               selectedItem={selectedItem}
               selectHandler={handleSelect}
+              isOpen={openPopover}
+              handleOpen={handleOpen}
             />
           }
-          panelContent={<EntityPanel classes="flex lg:hidden" type={entity.context.entityTypeName} id={entity.uniqueIdentifier} metadata={entity.metadata} />}
+          panelContent={
+            <EntityPanel
+              classes="flex lg:hidden"
+              type={entity.context.entityTypeName}
+              id={entity.uniqueIdentifier}
+              metadata={entity.metadata}
+            />
+          }
           onSwitchView={(view: string) => setView(view)}
           defaultView={view}
         />
-        { view === "cards" ? <CardList>
-          {associated.map((entity) =>
-            <Card key={entity.uniqueIdentifier} type={entity.context.entityTypeName} badgeText={entity.uniqueIdentifier} badgeIcon="reward">
-            <KeyValueList
-              entryLabels={Object.keys(entity.metadata)}
-              entries={Object.entries(entity.metadata)}
-            />
-          </Card>
-          )}
-        </CardList> : null }
-        { view === "table" ? <Table data={associated} onRowClick={(row)=>console.log(row)} /> : null }
+        {view === "cards" ? (
+          <CardList>
+            {associated.map((entity) => (
+              <Card
+                key={entity.uniqueIdentifier}
+                type={entity.context.entityTypeName}
+                badgeText={entity.uniqueIdentifier}
+                badgeIcon="reward"
+              >
+                <KeyValueList
+                  entryLabels={Object.keys(entity.metadata)}
+                  entries={Object.entries(entity.metadata)}
+                />
+              </Card>
+            ))}
+          </CardList>
+        ) : null}
+        {view === "table" ? (
+          <Table data={associated} onRowClick={(row) => console.log(row)} />
+        ) : null}
       </div>
-      <EntityPanel classes="sticky top-0 hidden lg:flex" type={entity.context.entityTypeName} id={entity.uniqueIdentifier} metadata={entity.metadata} />
+      <EntityPanel
+        classes="sticky top-0 hidden lg:flex"
+        type={entity.context.entityTypeName}
+        id={entity.uniqueIdentifier}
+        metadata={entity.metadata}
+      />
     </div>
   );
 }
