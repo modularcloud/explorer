441e38a00b941aa36661b6bd9567ea3c2b64d52f
lzrs <84213303+lzrscg@users.noreply.github.com>
2023-02-16 22:48:04 +0000

    improve search perf

--- apps/web/pages/api/path/[networkLabel]/[fieldValue].ts
+++ apps/web/pages/api/path/[networkLabel]/[fieldValue].ts
@@ -6,23 +6,24 @@ import { ServiceManager } from '../../../../lib/service-manager';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const { networkLabel, fieldValue } = req.query;
     if (typeof networkLabel !== "string") {
-        return res.status(404)
+        return res.status(404).end();
     }
     if (typeof fieldValue !== "string") {
-        return res.status(404)
+        return res.status(404).end()
     }
     const network = ServiceManager.getNetwork(networkLabel);
     if (!network) {
-        return res.status(404);
+        return res.status(404).end();
     }
 
     // Try height
     if (isHeight(fieldValue)) {
         const block = await getEntity(network, "block", "height", fieldValue);
         if (block) {
-            return res.json({
+            res.json({
                 path: `/${networkLabel}/block/height/${fieldValue}`,
             })
+            return res.status(200).end();
         }
     }
 
@@ -31,20 +32,23 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         // Try tx
         const transaction = await getEntity(network, "transaction", "hash", fieldValue);
         if (transaction) {
-            return res.json({
+            res.json({
                 path: `/${networkLabel}/transaction/hash/${fieldValue}`,
             });
+            return res.status(200).end();
+
         }
 
         // Try block
         const block = await getEntity(network, "block", "hash", fieldValue);
         if (block) {
-            return res.json({
+            res.json({
                 path: `/${networkLabel}/block/hash/${fieldValue}`,
             })
+            return res.status(200).end();
         }
     }
 
     // No luck
-    else res.status(404);
+    res.status(404).end();
 }
