edd85c2bdd658b469f14333ed646430546e225b6
lzrs <84213303+lzrscg@users.noreply.github.com>
2023-02-17 02:24:26 +0000

    fix footer text

--- packages/@modularcloud/design-system/components/footer/index.tsx
+++ packages/@modularcloud/design-system/components/footer/index.tsx
@@ -6,7 +6,7 @@ export function Footer({ children }: Props) {
   return (
     <footer className="flex flex-col justify-evenly text-center absolute bottom-0 border-t h-1/6 p-2">
       <span className="text-slate-900">
-        @Copyright {new Date().getFullYear() - 1}. All rights reserved.
+        &copy; Copyright {new Date().getFullYear()}. All rights reserved.
       </span>
       <div className="flex flex-row justify-around">{children}</div>
     </footer>
