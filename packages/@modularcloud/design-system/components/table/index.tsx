cb9b5aef17d5cf7dbd6c96e03ec97877fb8b9271
lzrs <84213303+lzrscg@users.noreply.github.com>
2023-02-22 20:59:51 +0000

    improve table view

--- packages/@modularcloud/design-system/components/table/index.tsx
+++ packages/@modularcloud/design-system/components/table/index.tsx
@@ -6,14 +6,14 @@ import {
 } from "@tanstack/react-table";
 import { Entity } from "service-manager/types/entity.type";
 import { ElipsHorizOff } from "../../icons";
-import clsx from 'clsx';
+import clsx from "clsx";
 import { Badge } from "../badge";
 import { Status } from "../status";
-import * as React from 'react';
+import * as React from "react";
 
 type Props = {
   data: Entity[];
-  router: any
+  router: any;
 };
 
 type EntityColumn<T extends React.ReactNode> = {
@@ -25,86 +25,129 @@ type EntityColumn<T extends React.ReactNode> = {
   isPrimaryKey?: boolean;
   isIcon?: boolean;
   getCell: (entity: Entity) => T;
-} 
+};
 
 type TableSection = {
   rows: Entity[];
   columns: EntityColumn<any>[];
   label: string;
-}
+};
 
 export function Table({ data, router }: Props) {
   // temporarily before we have multi-entity tables
-  if(!data.length) {
+  if (!data.length) {
     return null;
   }
   const type = data[0].context.entityTypeName;
-  const filterData = data.filter(entity => entity.context.entityTypeName === type);
-  const section: TableSection = type === "Transaction" ? {
-    rows: filterData,
-    label: "Transactions",
-    columns: [
-      {
-        id: "icon",
-        isIcon: true,
-        showOnXS: true,
-        getCell: (entity: Entity) => <Status status={entity.metadata.status} mode="icon" />
-      },
-      {
-        id: "hash",
-        header: "Transactions",
-        isPrimaryKey: true,
-        getCell: (entity: Entity) => entity.uniqueIdentifier
-      },
-      {
-        id: "type",
-        header: "Type",
-        rightJustifyOnXS: true,
-        getCell: (entity: Entity) => <Badge list={entity.computed.Messages?.map((message: any) => message.uniqueIdentifier)} />
-      },
-      {
-        id: "status",
-        header: "Status",
-        hideOnXS: true,
-        getCell: (entity: Entity) => <Status status={entity.metadata.Status} />
-      },
-      {
-        id: "menu",
-        isIcon: true,
-        getCell: (entity: Entity) => <ElipsHorizOff />
-      },
-    ]
-  }  : {
-    rows: filterData,
-    label: "Messages", // TODO: make generic
-    columns: [
-      /*{
+  const isEVM = !!(
+    data[0].context.network === "Triton" ||
+    data[0].context.network === "Ethereum"
+  );
+  const filterData = data.filter(
+    (entity) => entity.context.entityTypeName === type
+  );
+  let section: TableSection;
+  if (type === "Transaction") {
+    if (isEVM) {
+      section = {
+        rows: filterData,
+        label: "Transactions",
+        columns: [
+          {
+            id: "hash",
+            header: "Transactions",
+            isPrimaryKey: true,
+            getCell: (entity: Entity) => entity.uniqueIdentifier,
+          },
+          {
+            id: "menu",
+            isIcon: true,
+            getCell: (entity: Entity) => <ElipsHorizOff />,
+          },
+        ],
+      };
+    } else {
+      section = {
+        rows: filterData,
+        label: "Transactions",
+        columns: [
+          {
+            id: "icon",
+            isIcon: true,
+            showOnXS: true,
+            getCell: (entity: Entity) => (
+              <Status status={entity.metadata.status} mode="icon" />
+            ),
+          },
+          {
+            id: "hash",
+            header: "Transactions",
+            isPrimaryKey: true,
+            getCell: (entity: Entity) => entity.uniqueIdentifier,
+          },
+          {
+            id: "type",
+            header: "Type",
+            rightJustifyOnXS: true,
+            getCell: (entity: Entity) => (
+              <Badge
+                list={entity.computed.Messages?.map(
+                  (message: any) => message.uniqueIdentifier
+                )}
+              />
+            ),
+          },
+          {
+            id: "status",
+            header: "Status",
+            hideOnXS: true,
+            getCell: (entity: Entity) => (
+              <Status status={entity.metadata.Status} />
+            ),
+          },
+          {
+            id: "menu",
+            isIcon: true,
+            getCell: (entity: Entity) => <ElipsHorizOff />,
+          },
+        ],
+      };
+    }
+  } else if (type === "Message") {
+    section = {
+      rows: filterData,
+      label: "Messages", // TODO: make generic
+      columns: [
+        /*{
         id: "icon",
         isIcon: true,
         showOnXS: true,
         getCell: (entity: Entity) => <Status status={entity.metadata.status} mode="icon" />
       },*/
-      {
-        id: "id",
-        header: "Messages", // TODO: Make generic
-        isPrimaryKey: true,
-        getCell: (entity: Entity) => entity.uniqueIdentifier
-      },
-      {
-        id: "menu",
-        isIcon: true,
-        getCell: (entity: Entity) => <ElipsHorizOff />
-      },
-    ]
+        {
+          id: "id",
+          header: "Messages", // TODO: Make generic
+          isPrimaryKey: true,
+          getCell: (entity: Entity) => entity.uniqueIdentifier,
+        },
+        {
+          id: "menu",
+          isIcon: true,
+          getCell: (entity: Entity) => <ElipsHorizOff />,
+        },
+      ],
+    };
+  } else {
+    return null;
   }
-  
+
   const columnHelper = createColumnHelper<Entity>();
-  const columns = section.columns.map(col => {
+  const columns = section.columns.map((col) => {
     return columnHelper.accessor("uniqueIdentifier", {
       id: col.id,
       header: col.header ?? (() => null),
       cell: (info) => col.getCell(info.row.original),
-    })
+    });
   });
 
   const table = useReactTable<Entity>({
@@ -113,40 +156,108 @@ export function Table({ data, router }: Props) {
     getCoreRowModel: getCoreRowModel(),
   });
 
-  const maxXSLeftPadding = section.columns.findIndex(col => !col.hideOnXS);
-  const maxXSRightPadding = section.columns.length - 1 - [...section.columns].reverse().findIndex(col => !col.hideOnXS);
-  const minXSLeftPadding = section.columns.findIndex(col => !col.showOnXS);
-  const minXSRightPadding = section.columns.length - 1 - [...section.columns].reverse().findIndex(col => !col.showOnXS);
+  const maxXSLeftPadding = section.columns.findIndex((col) => !col.hideOnXS);
+  const maxXSRightPadding =
+    section.columns.length -
+    1 -
+    [...section.columns].reverse().findIndex((col) => !col.hideOnXS);
+  const minXSLeftPadding = section.columns.findIndex((col) => !col.showOnXS);
+  const minXSRightPadding =
+    section.columns.length -
+    1 -
+    [...section.columns].reverse().findIndex((col) => !col.showOnXS);
 
   return (
     <div className="w-full overflow-x-hidden">
-      <div className="xs:hidden border-b border-b-night-100 h-10 font-bold w-full px-4 flex items-center">{section.label}</div>
+      <div className="xs:hidden border-b border-b-night-100 h-10 font-bold w-full px-4 flex items-center">
+        {section.label}
+      </div>
       <table className="text-left w-full">
         <thead>
           {table.getHeaderGroups().map((headerGroup) => (
-            <tr className="max-xs:hidden border-b border-b-night-100 h-10 font-bold" key={headerGroup.id}>
+            <tr
+              className="max-xs:hidden border-b border-b-night-100 h-10 font-bold"
+              key={headerGroup.id}
+            >
               {headerGroup.headers.map((header, index) => {
                 const rules = section.columns[index];
-                return <th className={clsx(rules.showOnXS && "xs:hidden", rules.hideOnXS && "max-xs:hidden", rules.rightJustifyOnXS && "max-xs:flex max-xs:justify-end", index === minXSLeftPadding && "xs:pl-4 sm:pl-6 md:pl-8", index == minXSRightPadding && "xs:pr-4 sm:pr-6 md:pr-8", index === maxXSLeftPadding && "max-xs:pl-4", index == maxXSRightPadding && "max-xs:pr-4", (!rules.isPrimaryKey && !rules.isIcon) && "sm:w-[167px] md:w-[175px]", rules.isIcon && "w-5")} key={header.id}>
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
-                        header.column.columnDef.header,
-                        header.getContext()
+                return (
+                  <th
+                    className={clsx(
+                      rules.showOnXS && "xs:hidden",
+                      rules.hideOnXS && "max-xs:hidden",
+                      rules.rightJustifyOnXS &&
+                        "max-xs:flex max-xs:justify-end",
+                      index === minXSLeftPadding && "xs:pl-4 sm:pl-6 md:pl-8",
+                      index == minXSRightPadding && "xs:pr-4 sm:pr-6 md:pr-8",
+                      index === maxXSLeftPadding && "max-xs:pl-4",
+                      index == maxXSRightPadding && "max-xs:pr-4",
+                      !rules.isPrimaryKey &&
+                        !rules.isIcon &&
+                        "sm:w-[167px] md:w-[175px]",
+                      rules.isIcon && "w-5"
+                    )}
+                    key={header.id}
+                  >
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
                         )}
-                </th>
+                  </th>
+                );
               })}
             </tr>
-        ))}
+          ))}
         </thead>
         <tbody>
           {table.getRowModel().rows.map((row) => (
-            <tr className="border-b border-b-[#F0F0F1] hover:bg-[#08061505] cursor-pointer" key={row.id} onClick={() => row.original.context.network === "N/A" ? null : router.push(`/${row.original.context.network}/${row.original.context.entityTypeName}/${row.original.uniqueIdentifierLabel}/${row.original.uniqueIdentifier}`)}>
+            <tr
+              className="border-b border-b-[#F0F0F1] hover:bg-[#08061505] cursor-pointer"
+              key={row.id}
+              onClick={() =>
+                row.original.context.network === "N/A"
+                  ? null
+                  : router.push(
+                      `/${row.original.context.network}/${row.original.context.entityTypeName}/${row.original.uniqueIdentifierLabel}/${row.original.uniqueIdentifier}`
+                    )
+              }
+            >
               {row.getVisibleCells().map((cell, index) => {
                 const rules = section.columns[index];
-                return <td className={clsx("py-3 text-mid-dark", rules.showOnXS && "xs:hidden", rules.hideOnXS && "max-xs:hidden", rules.rightJustifyOnXS && "max-xs:flex max-xs:justify-end", index === minXSLeftPadding && "xs:pl-4 sm:pl-6 md:pl-8", index == minXSRightPadding && "xs:pr-4 sm:pr-6 md:pr-8", (index === maxXSLeftPadding || index == maxXSRightPadding) && "max-xs:px-4", (!rules.isPrimaryKey && !rules.isIcon) && "sm:w-[167px] md:w-[175px]", rules.isIcon && "w-5")} key={cell.id}>
-                  <div className={clsx(rules.isPrimaryKey && "max-sm:w-20 truncate")}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</div>
-                </td>
+                return (
+                  <td
+                    className={clsx(
+                      "py-3 text-mid-dark",
+                      rules.showOnXS && "xs:hidden",
+                      rules.hideOnXS && "max-xs:hidden",
+                      rules.rightJustifyOnXS &&
+                        "max-xs:flex max-xs:justify-end",
+                      index === minXSLeftPadding && "xs:pl-4 sm:pl-6 md:pl-8",
+                      index == minXSRightPadding && "xs:pr-4 sm:pr-6 md:pr-8",
+                      (index === maxXSLeftPadding ||
+                        index == maxXSRightPadding) &&
+                        "max-xs:px-4",
+                      !rules.isPrimaryKey &&
+                        !rules.isIcon &&
+                        "sm:w-[167px] md:w-[175px]",
+                      rules.isIcon && "w-5"
+                    )}
+                    key={cell.id}
+                  >
+                    <div
+                      className={clsx(
+                        rules.isPrimaryKey && "max-sm:w-20 truncate"
+                      )}
+                    >
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </div>
+                  </td>
+                );
               })}
             </tr>
           ))}
