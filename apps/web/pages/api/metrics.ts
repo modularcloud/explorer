29b8f83b3a81a4670747e3c612ea94b5cdb873fc
lzrs <84213303+lzrscg@users.noreply.github.com>
2023-03-28 16:17:55 +0000

    allow cors on metrics

--- apps/web/pages/api/metrics.ts
+++ apps/web/pages/api/metrics.ts
@@ -1,8 +1,32 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
-  ) {
-    return res.json(await fetch(process.env.METRICS_API_URL ?? "").then((response) => response.json()));
-  }
+const allowCors =
+  (fn: any) => async (req: NextApiRequest, res: NextApiResponse) => {
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+    );
+    if (req.method === "OPTIONS") {
+      res.status(200).end();
+      return;
+    }
+    return await fn(req, res);
+  };
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  return res.json(
+    await fetch(process.env.METRICS_API_URL ?? "").then((response) =>
+      response.json()
+    )
+  );
+};
+
+module.exports = allowCors(handler);
