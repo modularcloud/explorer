8cd016f55f2bc0e45044878480aa8868a4081f89
Bolaji Ahmad <56865496+bolajahmad@users.noreply.github.com>
2023-05-01 10:42:09 +0100

    pulled code and read code

--- apps/web/lib/utils.ts
+++ apps/web/lib/utils.ts
@@ -73,6 +73,7 @@ export function getWhitelabel(): Whitelabel {
         defaultNetwork: "triton",
         name: ["Naut", "Scan"],
         env: "nautilus",
+        subText: "The future is now",
       };
     case "caldera":
       return {
