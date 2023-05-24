aa281164c39546d5a32f813aad575e3eb16313ee
lzrs <84213303+lzrscg@users.noreply.github.com>
2023-05-24 01:09:04 +0000

    add entity overview

---
+++ apps/web/app/[network]/@rightpanel/loading.tsx
@@ -0,0 +1,11 @@
+export default function RightPanelLoadingFallback() {
+  return (
+    <div
+      className={
+        "h-full lg:h-screen flex flex-col space-y-10 px-6 py-7 lg:px-5 sm:px-9 bg-gray-100 shadow-inner overflow-auto scrollbar-thin scrollbar-thumb-mid-dark-500 scrollbar-track-transparent scrollbar-thumb-rounded-full scrollbar-track-rounded-full sticky top-0 hidden lg:flex w-80 xl:w-[27.875rem]"
+      }
+    >
+      Loading...
+    </div>
+  );
+}
