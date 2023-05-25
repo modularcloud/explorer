001bae9469f9009116ddc94dc8f09d11b3686e5c
Amnot <acyclicgraphs@gmail.com>
2023-05-25 00:07:15 +0000

    add express to contract verification file

--- apps/web/pages/api/contract-verification/fetch-contract.ts
+++ apps/web/pages/api/contract-verification/fetch-contract.ts
@@ -1,69 +1,79 @@
+import express from 'express';
+import bodyParser from 'body-parser';
 import Web3 from 'web3';
 import solc from 'solc';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Verification } from '@prisma/client';
+
+const app = express();
+
+// Middlewares
+app.use(bodyParser.json());
 
 // Initialize a Web3 instance connected to the Celestia-Mocha testnet
-// (assuming the RPC URL is stored in process.env.MOCHA_RPC)
 const web3 = new Web3(process.env.MOCHA_RPC);
 const prisma = new PrismaClient();
 
-// Step 1: Fetch the Contract from the Blockchain
-async function fetchContractBytecode(contractAddress: string): Promise<string> {
-  try {
-    const bytecode = await web3.eth.getCode(contractAddress);
-    console.log(bytecode);
-    return bytecode;
-  } catch (error) {
-    console.error(`Could not fetch contract bytecode: ${error}`);
-    throw error;
-  }
-}
-
 // Compile a Solidity contract
-const input = {
-  language: 'Solidity',
-  sources: {
-    'contract.sol': {
-      content: 'contract C { function f() public { } }',
-    },
-  },
-  settings: {
-    outputSelection: {
-      '*': {
-        '*': ['*'],
+const compileContract = (sourceCode: string) => {
+  const input = {
+    language: 'Solidity',
+    sources: {
+      'contract.sol': {
+        content: sourceCode,
       },
     },
-  },
-};
+    settings: {
+      outputSelection: {
+        '*': {
+          '*': ['*'],
+        },
+      },
+    },
+  };
 
-const compiledContract = JSON.parse(solc.compile(JSON.stringify(input)));
-const compiledBytecode = compiledContract.contracts['contract.sol']['C'].evm.bytecode.object;
+  const compiledContract = JSON.parse(solc.compile(JSON.stringify(input)));
+  return compiledContract.contracts['contract.sol']['C'].evm.bytecode.object;
+}
 
-// Fetch the bytecode of a specific contract
-const contractAddress = 'yourContractAddress';
-fetchContractBytecode(contractAddress)
-  .then(async (bytecodeOnChain) => {
-    console.log('Bytecode on chain:', bytecodeOnChain);
-    console.log('Compiled bytecode:', compiledBytecode);
+app.post('/verify', async (req: { body: { contractAddress: any; sourceCode: any; }; }, res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: { error: string; }): void; new(): any; }; }; json: (arg0: { message: string; record: Verification; }) => void; }) => {
+  const { contractAddress, sourceCode } = req.body;
 
-    // Step 3: Compare the bytecode on chain with the compiled bytecode
+  // Fetch the Contract from the Blockchain
+  const fetchContractBytecode = async (contractAddress: string): Promise<string> => {
+    try {
+      const bytecode = await web3.eth.getCode(contractAddress);
+      return bytecode;
+    } catch (error) {
+      console.error(`Could not fetch contract bytecode: ${error}`);
+      res.status(500).json({ error: `Could not fetch contract bytecode: ${error}` });
+    }
+  };
+
+  // Fetch the bytecode of the specific contract
+  try {
+    const bytecodeOnChain = await fetchContractBytecode(contractAddress);
+    const compiledBytecode = compileContract(sourceCode);
     const isVerified = bytecodeOnChain === compiledBytecode;
 
-    // Step 4: Store Verification Data in Vercel Postgres
-    try {
-      const record = await prisma.verification.create({
-        data: {
-          contractAddress,
-          isVerified,
-          sourceCode: input.sources['contract.sol'].content,
-          bytecode: compiledBytecode,
-        },
-      });
-      console.log(record);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      await prisma.$disconnect();
-    }
-  })
-  .catch((error) => console.error('An error occurred:', error));
+    // Store Verification Data in the database
+    const record = await prisma.verification.create({
+      data: {
+        contractAddress,
+        isVerified,
+        sourceCode,
+        bytecode: compiledBytecode,
+      },
+    });
+
+    // Send response
+    res.json({ message: 'Contract verification completed', record });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred during contract verification' });
+  } finally {
+    await prisma.$disconnect();
+  }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
