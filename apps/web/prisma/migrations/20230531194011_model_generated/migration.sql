642d697be228cecfcc6a9bac6df321bcfcb0bc02
Aswanth <aswanthpcg@gmail.com>
2023-06-01 01:38:00 +0530

    Moved prisma to a seperate folder removed unwanted npm packages

--- apps/web/prisma/migrations/20230531194011_model_generated/migration.sql
+++ apps/web/prisma/migrations/20230531194011_model_generated/migration.sql
@@ -1,12 +1,17 @@
+-- CreateEnum
+CREATE TYPE "VerificationStatus" AS ENUM ('FULL', 'PARTIAL');
+
 -- CreateTable
 CREATE TABLE "Verification" (
     "id" SERIAL NOT NULL,
     "contractAddress" VARCHAR(255) NOT NULL,
+    "chainID" TEXT NOT NULL,
     "isVerified" BOOLEAN NOT NULL,
     "sourceCode" TEXT NOT NULL,
     "bytecode" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "verificationStatus" "VerificationStatus" NOT NULL,
 
     CONSTRAINT "Verification_pkey" PRIMARY KEY ("id")
 );
