07c7c94f1b80fee1896f0fbb4d095d7d6e3d5212
lzrs <84213303+lzrscg@users.noreply.github.com>
2023-05-31 18:32:39 +0000

    update suspense boundary on page

--- apps/web/app/[network]/[type]/(standard)/[query]/page.tsx
+++ apps/web/app/[network]/[type]/(standard)/[query]/page.tsx
@@ -1,6 +1,5 @@
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
-import { Suspense } from "react";
 import { PageArchetype } from "../../../../../ecs/archetypes/page";
 import { asyncUseEntity } from "../../../../../ecs/hooks/use-entity/server";
 import { FetchLoadArgs, getWhitelabel } from "../../../../../lib/utils";
@@ -34,29 +33,20 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 }
 
 export default async function EntityPage({ params }: Props) {
-  const entity = await asyncUseEntity({
+  asyncUseEntity({
     resourcePath: params,
     archetype: PageArchetype,
+  }).then((entity) => {
+    if (!entity) notFound();
   });
-  if (!entity) notFound();
-
-  // temporary until we create a new component
-  const { attributes, asyncAttributes, entityTypeName, entityId } =
-    entity.components.sidebar.data;
 
   return (
     <>
-      <DescriptionList
-        attributes={attributes}
-        asyncAttributes={asyncAttributes}
-        title={`${entityTypeName} Information`}
-        subTitle={entityId}
-      />
+      {/* @ts-expect-error Async Server Component */}
+      <DescriptionList resourcePath={params} />
       {/* <Raw resourcePath={params} /> */}
-      <Suspense>
-        {/* @ts-expect-error Async Server Component */}
-        <Tabs params={params} />
-      </Suspense>
+      {/* @ts-expect-error Async Server Component */}
+      <Tabs params={params} />
     </>
   );
 }
--- apps/web/ui/description-list/index.tsx
+++ apps/web/ui/description-list/index.tsx
@@ -6,15 +6,10 @@ import { FetchLoadArgs } from "../../lib/utils";
 import { asyncUseEntity } from "../../ecs/hooks/use-entity/server";
 import { AttributesArchetype } from "../../ecs/archetypes/attributes";
 import { Suspense } from "react";
+import { PageArchetype } from "../../ecs/archetypes/page";
 
 type Props = {
-  title: string;
-  subTitle: string;
-  attributes: Record<string, Value>;
-  asyncAttributes?: {
-    src: FetchLoadArgs;
-    fallback: Record<string, Value>;
-  }[];
+  resourcePath: FetchLoadArgs;
 };
 
 function Entry({ label, value }: { label: string; value: Value }) {
@@ -84,21 +79,24 @@ async function AsyncEntries({ resourcePath }: { resourcePath: FetchLoadArgs }) {
   );
 }
 
-export default function DescriptionList({
-  title,
-  subTitle,
-  attributes,
-  asyncAttributes,
+export default async function DescriptionList({
+  resourcePath,
 }: Props) {
+  const entity = await asyncUseEntity({
+    resourcePath: resourcePath,
+    archetype: PageArchetype,
+  });
+  if (!entity) return null;
+  const { attributes, asyncAttributes, entityTypeName, entityId } = entity.components.sidebar.data;
   return (
     <div className="flex w-full p-6 justify-center">
       <div className="bg-translucent backdrop-blur-xs w-full overflow-hidden border border-mid-dark-100 shadow-[0px_3px_6px_rgba(42,43,46,_0.07),0px_1px_2px_rgba(42,43,46,0.04)] rounded-xl max-w-7xl">
         <div className=" p-6">
           <h3 className="text-base font-semibold leading-7 text-temp-900">
-            {title}
+            {`${entityTypeName} Information`}
           </h3>
           <p className="mt-1 max-w-2xl text-sm leading-6 text-temp-500 truncate">
-            {subTitle}
+            {entityId}
           </p>
         </div>
         <div className="mx-6">
