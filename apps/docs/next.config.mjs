87ef815ed02150498b21b4cefbba545b11fbf866
Harris Levine <acyclicgraphs@gmail.com>
2023-06-10 13:21:43 -0400

    add docs

---
+++ apps/docs/next.config.mjs
@@ -0,0 +1,25 @@
+import nextMDX from '@next/mdx'
+import withSearch from './src/mdx/search.mjs'
+import { remarkPlugins } from './src/mdx/remark.mjs'
+import { rehypePlugins } from './src/mdx/rehype.mjs'
+import { recmaPlugins } from './src/mdx/recma.mjs'
+
+const withMDX = nextMDX({
+  options: {
+    remarkPlugins,
+    rehypePlugins,
+    recmaPlugins,
+    providerImportSource: '@mdx-js/react',
+  },
+})
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+  experimental: {
+    scrollRestoration: true,
+  },
+}
+
+export default withSearch(withMDX(nextConfig))
---
+++ apps/docs/src/components/Logo.jsx
@@ -0,0 +1,10 @@
+export function Logo(props) {
+  return (
+    <span
+      href="/"
+      className="dark:text-white text-[1.125rem] font-[700]"
+    >
+      Modular Cloud
+    </span>
+  );
+}
