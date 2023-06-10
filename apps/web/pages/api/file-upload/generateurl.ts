46ede122ddaa343666c3a9b8cfc70ce5c1b2ae5c
Amnot <acyclicgraphs@gmail.com>
2023-06-10 12:43:35 +0000

    checks for query parameters file & contractaddress

--- apps/web/pages/api/file-upload/generateurl.ts
+++ apps/web/pages/api/file-upload/generateurl.ts
@@ -1,12 +1,17 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import generateUploadUrl from "./s3file-upload";
+
 export default async function generateS3Url(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const url = await generateUploadUrl(
-    req.query.file,
-    req.query.contractaddress
-  );
-  res.send({ url });
+  const file = req.query.file as string | undefined;
+  const contractaddress = req.query.contractaddress as string | undefined;
+
+  if (!file || !contractaddress) {
+    res.status(400).send('Bad Request: Missing required parameters.');
+  } else {
+    const url = await generateUploadUrl(file, contractaddress);
+    res.send({ url });
+  }
 }
