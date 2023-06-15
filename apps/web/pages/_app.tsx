8ab0a6893d0e2c6c94164b42a854c97f8739256d
Amnot <acyclicgraphs@gmail.com>
2023-06-15 21:56:57 +0000

    Revert "build page in /app directory"
    This reverts commit 0a7b8dce2cf7950b8392e8cb9dd3691d6d276ce4.

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,9 +1,7 @@
 import '../styles/globals.css';
 import { AppProps } from 'next/app';
-import { VerifyAndUpload } from '../ui/contract-verify/verifyAndUpload';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  <VerifyAndUpload />
   return <Component {...pageProps} />;
 }
 
