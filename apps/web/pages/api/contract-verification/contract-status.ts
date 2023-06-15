2f05afaa06b39f4bbb69a7e1e35b6b7b8d74b82f
Harris Levine <acyclicgraphs@gmail.com>
2023-06-14 22:37:55 -0400

    ensure contractAddress is always a single string

--- apps/web/pages/api/contract-verification/contract-status.ts
+++ apps/web/pages/api/contract-verification/contract-status.ts
@@ -1,10 +1,12 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../prisma/lib/prisma";
+
 export default async function isContractVerified(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const contractAddress = req.query.contractaddress;
+  const contractAddress = Array.isArray(req.query.contractaddress) ? req.query.contractaddress[0] : req.query.contractaddress;
+
   if (contractAddress) {
     try {
       const findContractAddress = await prisma.verification.findUnique({
