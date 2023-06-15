0a7b8dce2cf7950b8392e8cb9dd3691d6d276ce4
Amnot <acyclicgraphs@gmail.com>
2023-06-15 21:48:40 +0000

    build page in /app directory

--- apps/web/app/_app.tsx
+++ apps/web/app/_app.tsx
@@ -1,7 +1,9 @@
 import '../styles/globals.css';
 import { AppProps } from 'next/app';
+import { VerifyAndUpload } from '../ui/contract-verify/verifyAndUpload';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  <VerifyAndUpload />
   return <Component {...pageProps} />;
 }
 
