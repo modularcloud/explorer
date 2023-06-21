cbb53ff06e198a3af124cb94f09e4aba282fc423
Aswanth <aswanthpcg@gmail.com>
2023-06-22 00:42:14 +0530

    added unzip and read function to readfiles from cloud

--- apps/verify/pages/api/contract-verification/fetch-verified.ts
+++ apps/verify/pages/api/contract-verification/fetch-verified.ts
@@ -1,13 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../../prisma/lib/prisma";
+import axios, { AxiosResponse } from "axios";
+import unzipper, { Entry } from "unzipper";
+import { Verification } from ".prisma/client";
+
 export default async function FetchVerifiedContract(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const contractAddress = Array.isArray(req.query.contractaddress)
+  let contractAddress = Array.isArray(req.query.contractaddress)
     ? req.query.contractaddress[0]
     : req.query.contractaddress;
-
+  contractAddress = contractAddress.toLowerCase();
+  const readFiles = req.query.readfiles ?? null;
   if (contractAddress) {
     try {
       const findContractAddress = await prisma.verification.findUnique({
@@ -15,7 +20,19 @@ export default async function FetchVerifiedContract(
           contractAddress: contractAddress,
         },
       });
-      if (findContractAddress) {
+      if (findContractAddress && readFiles === "true") {
+        const url = findContractAddress.uploadedUrl;
+        await axios
+          .get(url, { responseType: "stream" })
+          .then(async (response: AxiosResponse<any>) => {
+            await unzipAndRead(response, findContractAddress);
+            res.status(200).json(findContractAddress);
+          })
+          .catch((error: Error) => {
+            console.log(error);
+            res.status(500).json(error);
+          });
+      } else if (findContractAddress) {
         res.status(200).json(findContractAddress);
       } else {
         res.status(200).json({ isVerified: false });
@@ -26,3 +43,35 @@ export default async function FetchVerifiedContract(
     }
   }
 }
+
+const unzipAndRead = (
+  response: AxiosResponse<any>,
+  findContractAddress: Verification
+) => {
+  return new Promise((resolve, reject) => {
+    findContractAddress.files = {};
+    response.data
+      .pipe(unzipper.Parse())
+      .on("entry", (entry: Entry) => {
+        const fileName = entry.path;
+        const type = entry.type; // 'Directory' or 'File'
+
+        if (type === "File") {
+          let content = "";
+          entry.on("data", (data: Buffer) => {
+            content += data.toString();
+          });
+          entry.on("end", () => {
+            findContractAddress.files[fileName] = content;
+          });
+        } else {
+          entry.autodrain();
+        }
+      })
+      .promise()
+      .then(
+        () => resolve(findContractAddress),
+        (e) => console.log("error", e)
+      );
+  });
+};
