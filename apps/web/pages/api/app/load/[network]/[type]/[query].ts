085b746e8494db813137ec4b2d37625d54820b25
lzrs <lzrs@modular.cloud>
2023-07-12 09:45:48 -0700

    temporarily allow cors

--- apps/web/pages/api/app/load/[network]/[type]/[query].ts
+++ apps/web/pages/api/app/load/[network]/[type]/[query].ts
@@ -3,10 +3,28 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { PageArchetype } from "../../../../../../ecs/archetypes/page";
 import { getEngine } from "../../../../../../lib/networks";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+const allowCors =
+  (fn: any) => async (req: NextApiRequest, res: NextApiResponse) => {
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+    );
+    if (req.method === "OPTIONS") {
+      res.status(200).end();
+      return;
+    }
+    return await fn(req, res);
+  };
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { config, Engine } = getEngine();
   try {
     // temporarily add address/token as search types until we can implement redirections
@@ -39,3 +57,5 @@ export default async function handler(
     res.status(404).end();
   }
 }
+
+module.exports = allowCors(handler);
