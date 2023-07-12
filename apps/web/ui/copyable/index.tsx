e37ca9b94e69000f3ad254f5a39acd1613c7094e
lzrs <lzrs@modular.cloud>
2023-07-12 13:09:55 -0700

    add link to copyable value

--- apps/web/ui/copyable/index.tsx
+++ apps/web/ui/copyable/index.tsx
@@ -1,26 +1,67 @@
 "use client";
 
+import { FetchLoadArgs } from "lib/utils";
 import { useToast } from "ui/shadcn/components/ui/use-toast";
+import { useRouter } from "next/navigation";
+import clsx from "clsx";
 
 type Props = {
   value: string | number,
+  link?: FetchLoadArgs | string,
 }
-export function CopyableValue({ value }: Props) {
+
+export function CopyableValue({ value, link }: Props) {
+  // Use hooks for toast notifications and routing
   const { toast } = useToast();
-  const copy = () => {
-    navigator.clipboard.writeText(value.toString())
-  }
+  const router = useRouter();
+
+  // Function to copy the value to the clipboard
+  const copy = async () => {
+    try {
+      await navigator.clipboard.writeText(value.toString());
+      return true;
+    } catch (err) {
+      console.error('Failed to copy text: ', err);
+      return false;
+    }
+  };
+
+  // Construct link from FetchLoadArgs
+  const constructLink = (link: FetchLoadArgs) => `${link.network}/${link.type}/${link.query}`;
+
+  // Function to navigate to the link
+  const open = () => {
+    if (link) {
+      const route = typeof link === 'string' ? link : constructLink(link);
+      router.push(route);
+    }
+  };
+
+  // Handler for onClick event
+  const onClickHandler = async (e: React.MouseEvent<HTMLSpanElement>) => {
+    e.stopPropagation();
+
+    if (e.ctrlKey || e.metaKey) {
+      open();
+    } else {
+      const copied = await copy();
+
+      if (copied) {
+        const val = String(value).length > 20 ? `${String(value).slice(0, 20)}...` : value;
+        toast({
+          title: "Copied",
+          description: `"${val}" copied to clipboard`,
+        });
+      }
+    }
+  };
+
   return (
-    <span onClick={(e) => {
-      e.stopPropagation();
-      const val = String(value).length > 20 ? String(value).slice(0, 20) + "..." : value;
-      copy();
-      toast({
-        title: "Copied",
-        description: `"${val}" copied to clipboard`,
-      })
-    }} className="cursor-pointer">
+    <span
+      onClick={onClickHandler}
+      className={clsx("cursor-pointer", link && "underline")}
+    >
       {value}
     </span>
-  )
+  );
 }
