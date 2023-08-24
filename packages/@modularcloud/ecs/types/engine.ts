2410df90c2e03156c74397e553294d722cb052bf
Adrien KISSIE <fredkiss3@gmail.com>
2023-08-25 01:57:33 +0200

    move docs comments on engine to make it easily discoverable

--- packages/@modularcloud/ecs/types/engine.ts
+++ packages/@modularcloud/ecs/types/engine.ts
@@ -27,6 +27,64 @@ export type LoadProps = {
 
 const _CONFIGS: Record<string, EngineConfig | MultiEngineConfig> = {};
 
+/**
+ * The engine used to fetch components and entities
+ * @example
+ *import { z } from "zod";
+ *import { createComponentSchema, Component } from "./component";
+ *import { ComponentTransform, createLoader } from "./etl";
+ *
+ *const a = z.object({ test: z.string() });
+ *const b = z.object({ tests: z.string().array() });
+ *
+ *const as = createComponentSchema(a, "hello");
+ *const bs = createComponentSchema(b, "world");
+ *
+ *const ac: Component<typeof a, "hello"> = {
+ *  typeId: "hello",
+ *  data: { test: "test1" },
+ *};
+ *const bc: Component<typeof b, "world"> = {
+ *  typeId: "world",
+ *  data: { tests: ["test2"] },
+ *};
+ *
+ *function fetch(endpoint: string) {
+ *  return Promise.resolve({
+ *    json: () => ({
+ *      test: "test1",
+ *      tests: ["test2"],
+ *    }),
+ *  });
+ *}
+ *
+ *const x: ComponentTransform<typeof as> = {
+ *  schema: as,
+ *  transform: async (data: unknown): Promise<z.infer<typeof as>> => {
+ *    return { typeId: "hello", data: { test: (data as { test: string }).test } };
+ *  },
+ *};
+ *const y: ComponentTransform<typeof bs> = {
+ *  schema: bs,
+ *  transform: async (data: unknown): Promise<z.infer<typeof bs>> => {
+ *    return {
+ *      typeId: "world",
+ *      data: { tests: (data as { tests: string[] }).tests },
+ *    };
+ *  },
+ *};
+ *
+ *const loaders = {
+ *  testLoader: createLoader()
+ *    .addExtract(async (endpoint: string, query: unknown) => {
+ *      return await fetch(endpoint).then((res) => res.json());
+ *    })
+ *    .addTransform(x)
+ *    .addTransform(y),
+ *};
+ *
+ * Engine.addConfig("tester", { endpoint: "localhost", loaders: loaders });
+ **/
 export const Engine = {
   addConfig: (name: string, config: EngineConfig | MultiEngineConfig) => {
     _CONFIGS[name] = config;
@@ -79,59 +137,3 @@ export const Engine = {
     }
   },
 };
-
-// Example
-/*import { z } from "zod";
-import { createComponentSchema, Component } from "./component";
-import { ComponentTransform, createLoader } from "./etl";
-
-const a = z.object({ test: z.string() });
-const b = z.object({ tests: z.string().array() });
-
-const as = createComponentSchema(a, "hello");
-const bs = createComponentSchema(b, "world");
-
-const ac: Component<typeof a, "hello"> = {
-  typeId: "hello",
-  data: { test: "test1" },
-};
-const bc: Component<typeof b, "world"> = {
-  typeId: "world",
-  data: { tests: ["test2"] },
-};
-
-function fetch(endpoint: string) {
-  return Promise.resolve({
-    json: () => ({
-      test: "test1",
-      tests: ["test2"],
-    }),
-  });
-}
-
-const x: ComponentTransform<typeof as> = {
-  schema: as,
-  transform: async (data: unknown): Promise<z.infer<typeof as>> => {
-    return { typeId: "hello", data: { test: (data as { test: string }).test } };
-  },
-};
-const y: ComponentTransform<typeof bs> = {
-  schema: bs,
-  transform: async (data: unknown): Promise<z.infer<typeof bs>> => {
-    return {
-      typeId: "world",
-      data: { tests: (data as { tests: string[] }).tests },
-    };
-  },
-};
-
-const loaders = {
-  testLoader: createLoader()
-    .addExtract(async (endpoint: string, query: unknown) => {
-      return await fetch(endpoint).then((res) => res.json());
-    })
-    .addTransform(x)
-    .addTransform(y),
-};
-
-Engine.addConfig("tester", { endpoint: "localhost", loaders: loaders });*/
