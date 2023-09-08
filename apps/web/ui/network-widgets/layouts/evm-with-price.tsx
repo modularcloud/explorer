a00915e5db47df7fcdd2f6d4bcbe5d69ed348618
Adrien KISSIE <fredkiss3@gmail.com>
2023-09-08 03:54:56 +0200

    refactor: remove unused import

--- apps/web/ui/network-widgets/layouts/evm-with-price.tsx
+++ apps/web/ui/network-widgets/layouts/evm-with-price.tsx
@@ -1,7 +1,6 @@
 "use client";
 import * as React from "react";
 import { useParams } from "next/navigation";
-import { Card } from "~/ui/card";
 import { cn } from "~/ui/shadcn/utils";
 import {
   BarChart,
