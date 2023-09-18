afb0426fdd5f8b984301a710bc73c21e26f0e89e
Adrien KISSIE <fredkiss3@gmail.com>
2023-09-19 00:29:48 +0200

    refactor: don't use forwardRef for card component

--- apps/web/ui/card/index.tsx
+++ apps/web/ui/card/index.tsx
@@ -7,19 +7,16 @@ interface Props {
   style?: React.CSSProperties;
 }
 
-export const Card = React.forwardRef<React.ElementRef<"div">, Props>(
-  function Card({ children, className, style }, ref) {
-    return (
-      <div
-        ref={ref}
-        style={style}
-        className={cn(
-          "border-mid-dark-100 w-full rounded-lg border shadow-sm bg-white p-4",
-          className,
-        )}
-      >
-        {children}
-      </div>
-    );
-  },
-);
+export function Card({ children, className, style }: Props) {
+  return (
+    <div
+      style={style}
+      className={cn(
+        "border-mid-dark-100 w-full rounded-lg border shadow-sm bg-white p-4",
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
+}
