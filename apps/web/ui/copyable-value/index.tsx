62c945353f153dd9bd5ac678004c8dcf5e34c01a
Fred <hello@fredkiss.dev>
2023-09-26 05:40:50 +0200

    show/hide icon on `<CopyableValue>`

--- apps/web/ui/copyable-value/index.tsx
+++ apps/web/ui/copyable-value/index.tsx
@@ -9,40 +9,57 @@ import { cn } from "~/ui/shadcn/utils";
 
 interface Props {
   value: string;
+  hideCopyIcon?: boolean;
 }
 
-export function CopyableValue({ value }: Props) {
+export function CopyableValue({ value, hideCopyIcon = false }: Props) {
   const { toast } = useToast();
   return (
     <span className="inline-flex gap-2 max-w-full w-full py-1 items-center">
-      <p
-        className="text-ellipsis whitespace-nowrap overflow-x-hidden flex-shrink flex-grow-0 max-w-full"
-        tabIndex={-1}
-      >
-        {value}
-      </p>
+      {!hideCopyIcon ? (
+        <>
+          <p
+            className="text-ellipsis whitespace-nowrap overflow-x-hidden flex-shrink flex-grow-0 max-w-full"
+            tabIndex={-1}
+          >
+            {value}
+          </p>
+          <Tooltip label="Copy value">
+            <button
+              className={cn(
+                "rounded-md p-2 inline-flex items-center justify-center",
+                "border ring-primary/40 border-transparent outline-none",
+                "hover:bg-mid-dark-100 focus:ring-2 focus:border-primary",
+              )}
+              onClick={async () => {
+                const copied = await copyValueToClipboard(value);
 
-      <Tooltip label="Copy value">
-        <button
-          className={cn(
-            "rounded-md p-2 inline-flex items-center justify-center",
-            "border ring-primary/40 border-transparent outline-none",
-            "hover:bg-mid-dark-100 focus:ring-2 focus:border-primary",
-          )}
-          onClick={async () => {
-            const copied = await copyValueToClipboard(value);
-
-            if (copied) {
-              toast({
-                title: "Copied",
-                description: `"${truncateHash(value)}" copied to clipboard`,
-              });
-            }
-          }}
-        >
-          <Copy aria-hidden="true" className="text-muted" />
-        </button>
-      </Tooltip>
+                if (copied) {
+                  toast({
+                    title: "Copied",
+                    description: `"${truncateHash(value)}" copied to clipboard`,
+                  });
+                }
+              }}
+            >
+              <Copy aria-hidden="true" className="text-muted" />
+            </button>
+          </Tooltip>
+        </>
+      ) : (
+        <Tooltip label="Copy value">
+          <button
+            className={cn(
+              "text-ellipsis whitespace-nowrap overflow-x-hidden max-w-full",
+              "flex-shrink flex-grow-0 py-1 px-2 rounded-md",
+              "border ring-primary/40 border-transparent outline-none",
+              "hover:bg-mid-dark-100 focus:ring-2 focus:border-primary",
+            )}
+          >
+            {value}
+          </button>
+        </Tooltip>
+      )}
     </span>
   );
 }
