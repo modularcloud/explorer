abc80fd382e0bbc8dfde60462376559b6562c1fd
Fred <hello@fredkiss.dev>
2023-09-27 03:57:15 +0200

    🐛 make tooltip a client component

--- apps/web/ui/tooltip/index.tsx
+++ apps/web/ui/tooltip/index.tsx
@@ -1,3 +1,4 @@
+"use client";
 import * as React from "react";
 import * as RadixTooltip from "@radix-ui/react-tooltip";
 import { cn } from "~/ui/shadcn/utils";
@@ -19,7 +20,7 @@ export function Tooltip({ children, label, delayInMS = 500 }: Props) {
               "will-change-[transform,opacity]",
               "data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade",
               "text-foreground select-none rounded-md bg-white px-3 py-2 leading-none shadow-md text-sm",
-              "border border-mid-dark-100",
+              "border border-mid-dark-100 z-[100]",
             )}
             sideOffset={5}
           >
