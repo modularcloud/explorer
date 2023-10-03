7d86aa650cf9709fd394dc9751a681a9c4f5f06f
aswanth-c <aswanthpcg@gmail.com>
2023-10-03 22:30:59 +0530

    added learnmore and linked docs site to it

--- apps/verify/pages/contract-verification/index.tsx
+++ apps/verify/pages/contract-verification/index.tsx
@@ -9,13 +9,23 @@ export default function ContractVerifier() {
   return (
     <div className={` ${inter.variable} font-sans `}>
       <div>
-        <p className="pt-14 xl:pt-20 text-center text-[#1D2939]  text-4xl  font-bold">
+        <p className="pt-14 text-center text-4xl font-bold  text-[#1D2939]  xl:pt-20">
           Verifier
         </p>
-        <p className="text-center pt-3 text-[#344054]">
-          Verify smart contracts by recompiling with the Solidity source code
-          and metadata
-        </p>
+        <div className="flex items-center justify-center pt-3 text-center text-[#344054]">
+          <p>
+            Verify smart contracts by recompiling with the Solidity source code
+            and metadata.
+          </p>
+          <a
+            href="https://docs.modular.cloud/"
+            target="_blank"
+            className="bold pl-1 text-[#7B6FE7] hover:cursor-pointer hover:underline"
+            rel="noreferrer"
+          >
+            Learn more
+          </a>
+        </div>
       </div>
       <div className="flex flex-col items-center">
         <VerifyAndUpload />
