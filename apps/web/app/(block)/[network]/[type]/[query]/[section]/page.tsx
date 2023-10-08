f066281d4473792bc146baa6650fbf6bb087f923
Fred <hello@fredkiss.dev>
2023-10-08 04:20:48 +0200

    🚧 WIP : section page

--- apps/web/app/(block)/[network]/[type]/[query]/[section]/page.tsx
+++ apps/web/app/(block)/[network]/[type]/[query]/[section]/page.tsx
@@ -1,5 +1,12 @@
 import * as React from "react";
-import { FetchLoadArgs } from "~/lib/shared-utils";
+import { notFound, redirect } from "next/navigation";
+import { PageArchetype } from "~/ecs/archetypes/page";
+import { fetchEntity } from "~/ecs/lib/server";
+import { slugify } from "~/lib/shared-utils";
+
+import type { FetchLoadArgs } from "~/lib/shared-utils";
+import type { Metadata } from "next";
+import { PaginationArchetype } from "~/ecs/archetypes/pagination";
 
 interface Props {
   params: FetchLoadArgs & {
@@ -7,10 +14,78 @@ interface Props {
   };
 }
 
-export default function Page({
-  params: { network, type, query, section },
-}: Props) {
-  return (
-    <h1 className="text-xl font-bold">{`${network}/${type}/${query}/${section}`}</h1>
-  );
+export default async function Page({ params }: Props) {
+  const { section, ...resourcePath } = params;
+  const entity = await fetchEntity({
+    resourcePath,
+    archetype: PageArchetype,
+  });
+  if (!entity) notFound();
+
+  const associated = entity.components.associated.data;
+  const labels = Object.keys(associated);
+
+  const currentSection =
+    labels.find((label) => slugify(label) === slugify(section ?? "")) ??
+    labels[0];
+
+  const collection = associated[currentSection];
+
+  let initialEntries: FetchLoadArgs[] = [];
+  let next: FetchLoadArgs | undefined;
+  if (collection.type === "static") {
+    initialEntries = collection.values;
+  }
+  if (collection.type === "paginated") {
+    const pagination = await fetchEntity({
+      resourcePath: collection.value,
+      archetype: PaginationArchetype,
+    });
+    if (pagination) {
+      initialEntries = pagination.components.pagination.data.values;
+      next = pagination.components.pagination.data.next;
+    }
+  }
+
+  return <></>;
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { section, ...resourcePath } = params;
+
+  // Redirect legacy url structure
+  if (
+    (resourcePath.type.toLowerCase() === "block" ||
+      resourcePath.type.toLowerCase() === "transaction" ||
+      resourcePath.type.toLowerCase() === "account") &&
+    (resourcePath.query.toLowerCase() === "hash" ||
+      resourcePath.query.toLowerCase() === "height" ||
+      resourcePath.query.toLowerCase() === "address")
+  ) {
+    redirect(
+      `/${resourcePath.network.toLowerCase()}/${resourcePath.type.toLowerCase()}/${section}`,
+    );
+  }
+
+  const entity = await fetchEntity({
+    resourcePath,
+    archetype: PageArchetype,
+  });
+  if (!entity)
+    return {
+      title: `Not Found - ModularCloud`,
+    };
+
+  const associated = entity.components.associated.data;
+  const labels = Object.keys(associated);
+
+  const label =
+    labels.find((label) => slugify(label) === slugify(section ?? "")) ??
+    labels[0];
+
+  return {
+    title: `${label} - ${entity.components.page.data.metadata.title}`,
+    description: entity.components.page.data.metadata.description,
+    keywords: entity.components.page.data.metadata.keywords,
+  };
 }
