b1e7939e15914ccae3279bc986fe28380771cd52
lzrs <lzrs@modular.cloud>
2023-10-13 14:05:33 -0700

    clean up right panel

--- apps/web/app/(block)/[network]/@rightpanel/[...path]/page.tsx
+++ apps/web/app/(block)/[network]/@rightpanel/[...path]/page.tsx
@@ -1,50 +1,29 @@
 import * as React from "react";
 import { Sidebar } from "~/ecs/components/sidebar";
-import { VMDisplayName, VMDisplayNames } from "~/lib/constants";
 import { getSingleNetworkCached } from "~/lib/network";
-import { Value } from "~/schemas/value";
 import { RightPanel } from "~/ui/right-panel";
-import { createSVMIntegration } from "@modularcloud/headless";
-
-import type { FetchLoadArgs } from "~/lib/shared-utils";
-import type { Page } from "@modularcloud/headless";
+import { loadPage, HeadlessRoute } from "~/lib/headless-utils";
+import { notFound } from "next/navigation";
 
 interface Props {
-  params: { network: string, path: string[] };
+  params: HeadlessRoute;
 }
 
-export default async function RightPanelShortenedPage({ params }: Props) {
+export default async function RightPanelPage({ params }: Props) {
   const network = await getSingleNetworkCached(params.network);
-
-  if (!network) return null;
-
-  if(!network.config.rpcUrls["svm"]) {
-    return null;
-}
-
-const integration = createSVMIntegration({
-    chainBrand: network.chainBrand,
-    chainName: network.chainName,
-    chainLogo: network.config.logoUrl,
-    entityType: "placeholder1",
-    entityQuery: "placeholder2",
-    rpcEndpoint: network.config.rpcUrls["svm"],
-    nativeToken: network.config.token.name
-});
-
-  const resolution = await integration.resolveRoute(params.path);
-  
-  if(!resolution || resolution.type !== "success") {
-    return null;
+  if (!network) {
+    notFound();
   }
-  const page: Page = resolution.result;
+
+  // TODO: consolidate the logic so that we don't need to call loadPage AND getSingleNetworkCached
+  const { sidebar } = await loadPage(params);
 
   const data: Sidebar = {
     logo: network.config.logoUrl,
-    entityTypeName: page.sidebar.headerKey,
-    entityId: page.sidebar.headerValue,
-    attributesHeader: `${page.sidebar.headerKey} Information`,
-    attributes: page.sidebar.properties,
+    entityTypeName: sidebar.headerKey,
+    entityId: sidebar.headerValue,
+    attributesHeader: `${sidebar.headerKey} Information`,
+    attributes: sidebar.properties,
   };
 
   return <RightPanel network={network} data={data} />;
