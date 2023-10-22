5288471d7e448fbaa904f1fd0e57ac817e2e52c9
Fred <hello@fredkiss.dev>
2023-10-22 03:17:46 +0200

    🚚 move (block) to (entity)

---
+++ apps/web/app/(entity)/[network]/[...path]/page.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import { Overview, OverviewSkeleton } from "~/ui/entity/overview";
+import { loadPage, HeadlessRoute } from "~/lib/headless-utils";
+import { Table } from "~/ui/entity/table";
+
+export async function generateMetadata({ params }: { params: HeadlessRoute }) {
+  const { metadata } = await loadPage(params);
+  return {
+    title: metadata.title,
+    description: metadata.description,
+  };
+}
+
+export default function EntityPage({ params }: { params: HeadlessRoute }) {
+  return (
+    <React.Suspense fallback={<OverviewSkeleton />}>
+      <AyncPageContent params={params} />
+    </React.Suspense>
+  );
+}
+
+async function AyncPageContent({ params }: { params: HeadlessRoute }) {
+  const page = await loadPage(params);
+
+  if (page.body.type === "notebook") {
+    return <Overview properties={page.body.properties} />;
+  }
+
+  return <Table columns={page.body.tableColumns} entries={page.body.entries} />;
+}
