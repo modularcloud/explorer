dfa1bfe0c8c6d08fc561677d35b948f3a92c886c
lzrs <lzrs@modular.cloud>
2023-11-09 05:12:29 -0800

    use proto-utils

--- packages/proto-utils/index.ts
+++ packages/proto-utils/index.ts
@@ -1,1 +1,7 @@
-export { Msgs as CelestiaMsgParsers } from "./msgs";
+import { Msgs } from "./msgs";
+import { Tx } from "./ts-out/celestiaorg_cosmos-sdk/cosmos/tx/v1beta1/tx";
+
+export const Celestia = {
+    Msgs,
+    Tx,
+}
