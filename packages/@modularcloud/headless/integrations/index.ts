31ca7ccd97fc8d0915d7a83da4cea206939df861
Fred <hello@fredkiss.dev>
2023-12-14 03:52:14 +0100

    💡 include IBC resolver

--- packages/@modularcloud/headless/integrations/index.ts
+++ packages/@modularcloud/headless/integrations/index.ts
@@ -30,7 +30,7 @@ import { RollappTransactionMessagesResolver } from "./rollapp/routes/transaction
 import { RollappMessageResolver } from "./rollapp/routes/transactions/[hash]/messages/[index]/page";
 import { IBCResolver } from "./rollapp/misc/ibc";
 import { RollAppAddressPageResolver } from "./rollapp/routes/addresses/[address]/page";
-// import { RollAppEvents } from "./rollapp/misc/hub";
+import { RollAppEvents } from "./rollapp/misc/hub";
 // import { resolvers } from "@modularcloud-resolver/rollapp";
 
 export function registerResolvers() {
@@ -68,7 +68,7 @@ export function registerResolvers() {
   registerResolver(RollappMessageResolver);
   registerResolver(IBCResolver);
   registerResolver(RollAppAddressPageResolver);
-  // registerResolver(RollAppEvents);
+  registerResolver(RollAppEvents);
   // TODO REMOVE
   // registerResolver(resolvers.getLatestHubMessages);
   // registerResolver(CelestiaAddressTransactionsResolver);
