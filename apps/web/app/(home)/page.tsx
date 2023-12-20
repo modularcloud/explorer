7fb25325172c91b1c6b93bc2e1197a90c9315056
Fred <hello@fredkiss.dev>
2023-12-20 03:18:11 +0100

    ♻️ SSG the homepage

--- apps/web/app/(home)/page.tsx
+++ apps/web/app/(home)/page.tsx
@@ -3,4 +3,4 @@ import { redirect } from "next/navigation";
 export default async function HomePage() {
   redirect("/celestia-mainnet");
 }
-export const runtime = "edge";
+// export const runtime = "edge";
