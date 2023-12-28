1a4a30144b23cfe2093a7f97d5cdfc810f8a35a5
Fred <hello@fredkiss.dev>
2023-12-28 02:58:35 +0100

    🍱 Add new checkmark icon

--- apps/web/ui/icons/index.ts
+++ apps/web/ui/icons/index.ts
@@ -17,6 +17,7 @@ export { default as Check } from "./Check";
 export { default as CheckCircle } from "./CheckCircle";
 export { default as CheckOff } from "./CheckOff";
 export { default as CheckOn } from "./CheckOn";
+export { default as Checkmark } from "./Checkmark";
 export { default as ChevronDown } from "./ChevronDown";
 export { default as Clock } from "./Clock";
 export { default as CloseOff } from "./CloseOff";
