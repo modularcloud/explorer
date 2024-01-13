38e37bad3fca5e5544fabde3e1b8846904f5a945
Fred <hello@fredkiss.dev>
2024-01-13 04:19:54 +0100

    🚧 WIP: electron POC

---
+++ apps/web/electron/index.mjs
@@ -0,0 +1,35 @@
+/* eslint-disable turbo/no-undeclared-env-vars */
+// @ts-check
+import { app, BrowserWindow } from "electron";
+
+import "../.next/standalone/apps/web/server.js";
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 900,
+    titleBarStyle: "hidden",
+    titleBarOverlay: {
+      color: "#2f3241",
+      symbolColor: "#74b1be",
+      height: 60,
+    },
+  });
+
+  mainWindow.loadURL(`http://localhost:3000`);
+
+  // if (!app.isPackaged) {
+  //   mainWindow.webContents.openDevTools();
+  // }
+  mainWindow.on("closed", () => {
+    app.quit();
+  });
+};
+
+app.whenReady().then(() => {
+  createWindow();
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
