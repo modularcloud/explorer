1ad47f646fab7e4762d8177c688226b3d97de7f4
Fred <hello@fredkiss.dev>
2024-01-19 03:23:15 +0100

    🩹 workaround for unsuccesfull or pending data

--- apps/web/lib/headless-utils.ts
+++ apps/web/lib/headless-utils.ts
@@ -14,7 +14,6 @@ import { nextCache } from "./server-utils";
 import { CACHE_KEYS } from "./cache-keys";
 import { z } from "zod";
 
-
 /**
  * This is reused on the `api/load-page/route.ts` file
  */
@@ -98,6 +97,11 @@ export async function loadIntegration(
             path,
             additionalContext,
           );
+
+          if (response === null || response.type !== "success") {
+            throw new Error("Not found");
+          }
+
           if (!includeTrace && response !== null) {
             const { trace, ...rest } = response;
             return rest;
@@ -145,27 +149,31 @@ export async function loadPage({
 
   const fixedPath = parseHeadlessRouteVercelFix(route).path;
 
-  const resolution = await integration.resolveRoute(fixedPath, context);
+  try {
+    const resolution = await integration.resolveRoute(fixedPath, context);
 
-  if (!resolution) {
+    if (!resolution) {
+      notFound();
+    }
+
+    if (resolution.type === "pending") {
+      /**
+       * Pending responses are for items that cannot be found, but may exist in the future.
+       * For example, if the latest block is 100, and we request block 101, we will get a pending response.
+       * Therefore, in the short-term we will treat this as any other page that is not found.
+       * However, we will have a special treatment for this in the future.
+       */
+      notFound();
+    }
+
+    if (resolution.type === "error") {
+      throw new Error(resolution.error);
+    }
+
+    return resolution.result as Page;
+  } catch (error) {
     notFound();
   }
-
-  if (resolution.type === "pending") {
-    /**
-     * Pending responses are for items that cannot be found, but may exist in the future.
-     * For example, if the latest block is 100, and we request block 101, we will get a pending response.
-     * Therefore, in the short-term we will treat this as any other page that is not found.
-     * However, we will have a special treatment for this in the future.
-     */
-    notFound();
-  }
-
-  if (resolution.type === "error") {
-    throw new Error(resolution.error);
-  }
-
-  return resolution.result as Page;
 }
 
 export async function search(networkSlug: string, query: string) {
