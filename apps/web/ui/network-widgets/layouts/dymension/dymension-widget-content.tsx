3073c4921a1a76e133a5af254e4e40e2b02a3139
Fred <hello@fredkiss.dev>
2024-02-06 05:19:12 +0100

    💄 animated event cards

--- apps/web/ui/network-widgets/layouts/dymension/dymension-widget-content.tsx
+++ apps/web/ui/network-widgets/layouts/dymension/dymension-widget-content.tsx
@@ -1,4 +1,5 @@
 "use client";
+import * as React from "react";
 import {
   IBCTransferEventCard,
   IBCTransferEventCardSkeleton,
@@ -7,7 +8,8 @@ import type { IBCTransferEvent } from "~/lib/dymension-utils";
 import { useDymensionWidgetData } from "./use-widget-data";
 import { useParams } from "next/navigation";
 import { range } from "~/lib/shared-utils";
-import { useClientOnlyTime } from "../../use-client-only-time";
+import { useClientOnlyTime } from "~/ui/network-widgets/use-client-only-time";
+import { Transition } from "@headlessui/react";
 
 export type DymensionWidgetContentProps = {
   initialEvents: IBCTransferEvent[];
@@ -25,6 +27,48 @@ export function DymensionWidgetContent({
   });
 
   const lastUpdatedTime = useClientOnlyTime(initialUpdatedAt, [data]);
+
+  const [items, setItems] = React.useState<
+    (IBCTransferEvent & { show: boolean })[]
+  >(() =>
+    initialEvents.map((event) => ({
+      ...event,
+      show: true,
+    })),
+  );
+
+  React.useEffect(() => {
+    setItems((oldEvents) => {
+      const newEvents = [...oldEvents];
+      const newData = data ?? [];
+
+      // hide events that aren't in the new data returned from the API
+      // we want to limit to only 5 items shown at a time
+      for (const event of newEvents) {
+        if (!newData.find((ev) => ev.hash === event.hash)) {
+          event.show = false;
+        }
+      }
+      for (const event of newData) {
+        if (!newEvents.find((ev) => ev.hash === event.hash)) {
+          newEvents.push({
+            ...event,
+            show: true,
+          });
+        }
+      }
+
+      return newEvents.sort((a, b) => {
+        // put showed events before every other item
+        if (a.show) {
+          if (a.timestamp > b.timestamp) return -1;
+          if (b.timestamp > a.timestamp) return 1;
+        }
+        if (b.show) return 1;
+        return 0;
+      });
+    });
+  }, [data]);
 
   if (!data) {
     return <DymensionWidgetSkeleton error={error.toString()} />;
@@ -49,13 +93,33 @@ export function DymensionWidgetContent({
         )}
       </div>
       <ul>
-        {data.map((event, index) => (
-          <li key={event.hash} className="flex flex-col items-center">
-            <IBCTransferEventCard networkSlug={params.network} event={event} />
-            {index < data.length - 1 && (
-              <div aria-hidden="true" className="w-[1px] bg-mid-dark-100 h-4" />
-            )}
-          </li>
+        {items.map((event, index) => (
+          <Transition
+            as="li"
+            appear
+            className="flex flex-col items-center"
+            show={event.show}
+            key={event.hash}
+            enter="transform transition duration-500"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transform transition ease-in-out duration-700"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <>
+              <IBCTransferEventCard
+                networkSlug={params.network}
+                event={event}
+              />
+              {event.show && index < 4 && (
+                <div
+                  aria-hidden="true"
+                  className="w-[1px] bg-mid-dark-100 h-4"
+                />
+              )}
+            </>
+          </Transition>
         ))}
       </ul>
     </div>
