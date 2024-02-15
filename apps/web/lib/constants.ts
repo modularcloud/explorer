8be7f92413189715db18268c61d6ef5fee3cd81d
Fred <hello@fredkiss.dev>
2024-02-15 23:38:05 +0100

    ♻️ cleanup unused constants

--- apps/web/lib/constants.ts
+++ apps/web/lib/constants.ts
@@ -1,14 +1,4 @@
 export const DEFAULT_BRAND_COLOR = "256 100% 57%"; // in HSL format, corresponding to this color : #8457FF
-export const ENTITY_INDEX_TAB_NAME = "Overview"; // the default tab inside of entity pages
-
-// Supported Virtual machines
-export const VMDisplayNames = {
-  evm: "Ethereum Virtual Machine",
-  cosmos: "Cosmos SDK",
-} as const;
-
-export type VMDisplayName =
-  (typeof VMDisplayNames)[keyof typeof VMDisplayNames];
 
 export const DEFAULT_WIDGET_REVALIDATE_TIME_IN_SECONDS = 1;
 export const DEFAULT_WIDGET_REFETCH_TIME_IN_SECONDS = 5;
