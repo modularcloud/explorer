111182edcf3edf1cdc158fb117e4bbd561db087a
Fred <hello@fredkiss.dev>
2024-02-20 03:40:07 +0100

    ⚗️ test with time api

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -60,6 +60,28 @@ export default async function RootLayout({
   const searchOptionGroups = await getGroupedNetworkChains();
   console.timeEnd(`[${date}] \`getGroupedNetworkChains()\` from RootLayout`);
 
+  console.time(`[${date}] FETCH ["DATE_API_RESULT"]`);
+  const dateApiResult = await fetch(
+    "http://worldtimeapi.org/api/timezone/Europe/Paris",
+    {
+      cache: "force-cache",
+      next: {
+        tags: ["DATE_API_RESULT"],
+      },
+    },
+  ).then(
+    (r) =>
+      r.json() as Promise<{
+        unixtime: number;
+        utc_datetime: string;
+      }>,
+  );
+  console.timeEnd(`[${date}] FETCH ["DATE_API_RESULT"]`);
+  console.log({
+    unixtime: dateApiResult.unixtime,
+    utc_datetime: dateApiResult.utc_datetime,
+  });
+
   return (
     <html lang="en" dir="ltr" suppressHydrationWarning>
       <head>
