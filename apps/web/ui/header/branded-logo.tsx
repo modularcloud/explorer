7b2c288d874937ea50ff367e8da1a01754a8ae44
Fred <hello@fredkiss.dev>
2024-03-01 02:35:03 +0100

    💄 add link on the header

--- apps/web/ui/header/branded-logo.tsx
+++ apps/web/ui/header/branded-logo.tsx
@@ -5,6 +5,7 @@ import { useParams } from "next/navigation";
 import type { HeadlessRoute } from "~/lib/headless-utils";
 import { MCLogo } from "~/ui/mc-logo";
 import { useGroupedNetworksContext } from "~/ui/grouped-networks-context";
+import Link from "next/link";
 
 export function BrandedLogo() {
   const params = useParams() as Pick<HeadlessRoute, "network">;
@@ -18,14 +19,16 @@ export function BrandedLogo() {
   }, [optionGroups, params.network]);
 
   return (
-    <div
-      className="rounded-md p-1.5 flex flex-none items-center justify-center border border-primary"
-      style={{
-        "--color-primary": network.brandColor,
-        backgroundImage: network.brandCSSGradient.replaceAll(";", ""),
-      }}
-    >
-      <MCLogo className="h-4 w-4" />
-    </div>
+    <Link href="/">
+      <div
+        className="rounded-md p-1.5 flex flex-none items-center justify-center border border-primary"
+        style={{
+          "--color-primary": network.brandColor,
+          backgroundImage: network.brandCSSGradient.replaceAll(";", ""),
+        }}
+      >
+        <MCLogo className="h-4 w-4" />
+      </div>
+    </Link>
   );
 }
