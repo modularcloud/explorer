f77e668e98ab1be21e6bb27908dd217de3e6f170
Fred <hello@fredkiss.dev>
2024-03-13 04:15:12 +0100

    🔥 remove the vercel-api (it will not be used)

--- apps/vercel-api/server/api/get-dymension-ibc-events.get.ts
+++
@@ -1,6 +0,0 @@
-import { getDymensionIBCTransfertEvents } from "~/lib/dymension-utils";
-
-export default eventHandler(async () => {
-  const events = await getDymensionIBCTransfertEvents();
-  return Response.json(events);
-});
