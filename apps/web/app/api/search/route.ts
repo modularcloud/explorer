b934da29d3650093cae1ca6f84342b0ab5194f0b
Fred <hello@fredkiss.dev>
2024-03-16 03:53:50 +0100

    :rewind: restore search api route to `canary` to avoid merge conflicts

--- apps/web/app/api/search/route.ts
+++ apps/web/app/api/search/route.ts
@@ -32,7 +32,7 @@ export async function GET(req: NextRequest) {
     (searchBuilder) => searchBuilder.getPath(query)!,
   ).filter(Boolean);
 
-  const resolvedPathsPromise = await Promise.allSettled(
+  const resolvedPathsPromise = await Promise.all(
     queries.map((query) =>
       integration.resolveRoute(query).then((resolution) => {
         if (resolution?.type === "success") {
@@ -44,15 +44,7 @@ export async function GET(req: NextRequest) {
     ),
   );
 
-  const resolvedPaths = resolvedPathsPromise
-    .filter((result) => {
-      if (result.status === "rejected") {
-        return false;
-      } else {
-        return result.value !== null;
-      }
-    })
-    .map((res) => (res.status === "fulfilled" ? res.value : null)!);
+  const resolvedPaths = resolvedPathsPromise.filter((path) => path !== null);
 
   // remove duplicates
   const newPaths = resolvedPaths.reduce(
